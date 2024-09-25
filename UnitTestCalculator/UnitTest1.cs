5124f292ede8bc9469a0b37d20c89cb391c54aaf
david kurman <dividiak@gmail.com>
2024-09-25 19:17:05 +0300

    create Calculator class and Add function with test

---
+++ UnitTestCalculator/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using exercises_barak;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Given_TwoNumbersAndAdditionOperation_When_Calculating_Then_ReturnsSum()
+        {
+            var calculator = new Calculator();
+            double num1 = 2;
+            double num2 = 3;
+
+            var result = calculator.Add(num1, num2);
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ exercises_barak/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercises_barak
+{
+    public class Calculator
+    {
+        public double Calculate(double num1, double num2, string operation)
+        {
+            if (num1 < int.MinValue || num1 > int.MaxValue || num2 < int.MinValue || num2 > int.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException("One or two of the numbers are out of the range");
+            }
+
+            switch (operation)
+            {
+                case "+":
+                    return Add(num1,num2);
+                default:
+                    throw new InvalidOperationException("Invalid operation");
+            }
+        }
+        public double Add(double num1, double num2)
+        {
+            throw null;
+        }
+
+    }
+}
