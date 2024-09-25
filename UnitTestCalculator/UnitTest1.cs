1d37cba4a9bad11ccf93b3ad95e5c6b93bf06d5b
david kurman <dividiak@gmail.com>
2024-09-25 19:30:51 +0300

    Create tests for Sub & Multiply & Divide

--- UnitTestCalculator/UnitTest1.cs
+++ UnitTestCalculator/UnitTest1.cs
@@ -18,5 +18,38 @@ namespace UnitTestCalculator
 
             Assert.AreEqual(5, result);
         }
+        [TestMethod]
+        public void Given_TwoNumbersAndSubtractionOperation_When_Calculating_Then_ReturnsDifference()
+        {
+            var calculator = new Calculator();
+            double num1 = 8;
+            double num2 = 3;
+
+            var result = calculator.Sub(num1, num2);
+
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void Given_TwoNumbersAndMultiplicationOperation_When_Calculating_Then_ReturnsProduct()
+        {
+            var calculator = new Calculator();
+            double num1 = 2;
+            double num2 = 3;
+
+            var result = calculator.Multiply(num1, num2);
+
+            Assert.AreEqual(6, result);
+        }
+        [TestMethod]
+        public void Given_TwoNumbersAndDivisionOperation_When_Calculating_Then_ReturnsQuotient()
+        {
+            var calculator = new Calculator();
+            double num1 = 10;
+            double num2 = 2;
+
+            var result = calculator.Add(num1, num2);
+
+            Assert.AreEqual(5, result);
+        }
     }
 }
